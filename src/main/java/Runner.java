e2b4a0ba96d62e99b3dadddcdd55e9d62b1047d3
Eduardo Tocco <eduardo.tocco1@gmail.com>
2023-02-09 19:46:52 +0000

    Completed Lab Static Keyword

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,5 +1,5 @@
 
-public class Runner {
+public class Runner extends Class {
     /**
      * This challenge is about interacting with static variables.
      *
@@ -21,7 +21,8 @@ public class Runner {
      *
      * @param in the String value you should change global_var to in.
      */
-    public void changeClassVar(String in){
-
+    public void changeClassVar(String in)
+    {
+        global_var = in;
     }
 }
